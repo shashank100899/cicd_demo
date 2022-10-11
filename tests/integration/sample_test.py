ebf1f82119d458290673542e8f0debf5a2f3fc69
shashank <shashank1@TIGER03366.tigeranalytics.local>
2022-10-11 18:47:25 +0530

    Updated the sample_test.py file

--- tests/integration/sample_test.py
+++ tests/integration/sample_test.py
@@ -1,5 +1,13 @@
 import unittest
 
+import mlflow
+import mlflow.sklearn
+from sklearn.datasets import load_iris
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+from mlflow.tracking.client import MlflowClient
+
 from cicd_demo.jobs.sample.entrypoint import SampleJob
 from uuid import uuid4
 from pyspark.dbutils import DBUtils  # noqa
@@ -31,6 +39,37 @@ class SampleJobIntegrationTest(unittest.TestCase):
         self.dbutils.fs.rm(self.test_dir, True)
 
 
+class demo_model():
+    def __init__(self):
+        self.x = load_iris().data
+        self.y = load_iris().target
+    def model_build(self):
+        x_train , x_test , y_train , y_test = train_test_split(self.x,self.y,test_size = 0.3)
+        with mlflow.start_run(run_name = "cicd_demo"):
+            model = DecisionTreeClassifier(max_depth = 1 , random_state = 10)
+            model.fit(x_train , y_train) 
+            predicted = model.predict(x_test)
+            accuracy = metrics.accuracy_score(y_test , predicted)
+            print(accuracy)
+            mlflow.log_param("random_state" , 10)
+            mlflow.log_param("max_depth" , 1)
+            mlflow.log_metric("accuarcy" , accuracy)
+            mlflow.sklearn.log_model(model , "decision_tree")
+            modelpath = "/dbfs/mlflow/iris/model-%s-%f" % ("decision_tree" , 2)
+            mlflow.sklearn.save_model(model,modelpath)
+            run_id  = mlflow.active_run().info.run_id
+        
+        model_name = "CICD_IRIS_model"
+        model_uri = "runs:/{run_id}/{artifact_path}".format(run_id = run_id , artifact_path = "decision_tree")
+        model_details = mlflow.register_model(model_uri = model_uri , name = model_name)
+
+        client = MlflowClient()
+        client.transition_model_version_stage(
+                    model_details.name , 
+                    model_details.version,
+                    stage = "Production")
+
+
 if __name__ == "__main__":
     # please don't change the logic of test result checks here
     # it's intentionally done in this way to comply with jobs run result checks
@@ -43,3 +82,6 @@ if __name__ == "__main__":
         raise RuntimeError(
             "One or multiple tests failed. Please check job logs for additional information."
         )
+    model_object = demo_model()
+    model_object.model_build()
+
